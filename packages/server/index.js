c6617fecdab41cb163332de8184eaf8297e5a396
kakuhiry <gilbertbatista.k@gmail.com>
2021-11-10 13:37:24 -0400

    Test

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,7 +1,3 @@
 const commonFunction = require("@walnut/common");
-<<<<<<< HEAD
 console.log("also changed-06 - FINAL2");
-=======
-console.log("also changed-04 - FINAL2");
->>>>>>> new-version
 commonFunction();
